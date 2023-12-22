fbbce7e79f5fbeb85b413c785c619c2aef16a08c
Wloner0809 <wloner@mail.ustc.edu.cn>
2023-12-22 08:41:02 +0800

    format the code file

--- Sample_code/set.c
+++ Sample_code/set.c
@@ -6,92 +6,90 @@
 /*
 合并两个链表s1和s2并返回
 */
-symset uniteset(symset s1, symset s2)		
+symset uniteset(symset s1, symset s2)
 {
-	symset s;
-	snode* p;
-	
-	s1 = s1->next;
-	s2 = s2->next;
-	
-	s = p = (snode*) malloc(sizeof(snode));
-	while (s1 && s2)
-	{
-		p->next = (snode*) malloc(sizeof(snode));
-		p = p->next;
-		if (s1->elem < s2->elem)
-		{
-			p->elem = s1->elem;
-			s1 = s1->next;
-		}
-		else
-		{
-			p->elem = s2->elem;
-			s2 = s2->next;
-		}
-	}
+    symset s;
+    snode *p;
 
-	while (s1)
-	{
-		p->next = (snode*) malloc(sizeof(snode));
-		p = p->next;
-		p->elem = s1->elem;
-		s1 = s1->next;
-		
-	}
+    s1 = s1->next;
+    s2 = s2->next;
 
-	while (s2)
-	{
-		p->next = (snode*) malloc(sizeof(snode));
-		p = p->next;
-		p->elem = s2->elem;
-		s2 = s2->next;
-	}
+    s = p = (snode *)malloc(sizeof(snode));
+    while (s1 && s2)
+    {
+        p->next = (snode *)malloc(sizeof(snode));
+        p = p->next;
+        if (s1->elem < s2->elem)
+        {
+            p->elem = s1->elem;
+            s1 = s1->next;
+        }
+        else
+        {
+            p->elem = s2->elem;
+            s2 = s2->next;
+        }
+    }
 
-	p->next = NULL;
+    while (s1)
+    {
+        p->next = (snode *)malloc(sizeof(snode));
+        p = p->next;
+        p->elem = s1->elem;
+        s1 = s1->next;
+    }
 
-	return s;
+    while (s2)
+    {
+        p->next = (snode *)malloc(sizeof(snode));
+        p = p->next;
+        p->elem = s2->elem;
+        s2 = s2->next;
+    }
+
+    p->next = NULL;
+
+    return s;
 } // uniteset
-
 
 /*
 向链表中插入一个节点，值为elem，并保持链表的升序排序
 */
 void setinsert(symset s, int elem)
 {
-	snode* p = s;
-	snode* q;
+    snode *p = s;
+    snode *q;
 
-	while (p->next && p->next->elem < elem)
-	{
-		p = p->next;
-	}
-	
-	q = (snode*) malloc(sizeof(snode));
-	q->elem = elem;
-	q->next = p->next;
-	p->next = q;
+    while (p->next && p->next->elem < elem)
+    {
+        p = p->next;
+    }
+
+    q = (snode *)malloc(sizeof(snode));
+    q->elem = elem;
+    q->next = p->next;
+    p->next = q;
 } // setinsert
 
 /*
 传入任意个参数elem创建有序链表，并以SYM_NULL作为结束标志，例如symset linklist = createset(1,2,SYM_NULL);
 */
-symset createset(int elem, .../* SYM_NULL */)
+symset createset(int elem, ... /* SYM_NULL */)
 {
-	va_list list;
-	symset s;
+    va_list list;
+    symset s;
 
-	s = (snode*) malloc(sizeof(snode));
-	s->next = NULL;
+    s = (snode *)malloc(sizeof(snode));
+    s->next = NULL;
 
-	va_start(list, elem);
-	while (elem)
-	{
-		setinsert(s, elem);
-		elem = va_arg(list, int);
-	}
-	va_end(list);
-	return s;
+    va_start(list, elem);
+    while (elem)
+    {
+        setinsert(s, elem);
+        elem = va_arg(list, int);
+    }
+    va_end(list);
+    return s;
 } // createset
 
 /*
@@ -99,15 +97,15 @@ symset createset(int elem, .../* SYM_NULL */)
 */
 void destroyset(symset s)
 {
-	snode* p;
+    snode *p;
 
-	while (s)
-	{
-		p = s;
-		p->elem = -1000000;
-		s = s->next;
-		free(p);
-	}
+    while (s)
+    {
+        p = s;
+        p->elem = -1000000;
+        s = s->next;
+        free(p);
+    }
 } // destroyset
 
 /*
@@ -115,14 +113,14 @@ void destroyset(symset s)
 */
 int inset(int elem, symset s)
 {
-	s = s->next;
-	while (s && s->elem < elem)
-		s = s->next;
+    s = s->next;
+    while (s && s->elem < elem)
+        s = s->next;
 
-	if (s && s->elem == elem)
-		return 1;
-	else
-		return 0;
+    if (s && s->elem == elem)
+        return 1;
+    else
+        return 0;
 } // inset
 
 // EOF set.c
--- Sample_code/set.h
+++ Sample_code/set.h
@@ -3,13 +3,13 @@
 
 typedef struct snode
 {
-	int elem;
-	struct snode* next;
+    int elem;
+    struct snode *next;
 } snode, *symset;
 
 symset phi, declbegsys, statbegsys, facbegsys, relset;
 
-symset createset(int data, .../* SYM_NULL */);
+symset createset(int data, ... /* SYM_NULL */);
 void destroyset(symset s);
 symset uniteset(symset s1, symset s2);
 int inset(int elem, symset s);
